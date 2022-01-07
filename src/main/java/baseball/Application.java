a36fc954610d98b1cce335c0d4d8b12c42884963
1101717 <a1101717@1101717M01.local>
2022-01-07 15:43:06 +0900

    feat: add invalidate function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -21,6 +21,9 @@ public class Application {
 
             // 1 이상 9 이하의 임의의 수 생성
             int computerNumber = Randoms.pickNumberInRange(123, 987);
+            while (!checkInvalidate(computerNumber)) {
+                computerNumber = Randoms.pickNumberInRange(123, 987);
+            }
 
             // 사용자 수 입력
             String userInput = "ERROR";
@@ -39,21 +42,47 @@ public class Application {
 
     }
 
+    // 123 이상 987 이하 & 서로 다른 세 수인지 & 0 포함 여부 확인
+    private static boolean checkInvalidate(int input) {
+        if (123 > input || input > 987) {
+            return false;
+        }
+        int[] arrayNum = new int[10];
+        while (input != 0) {
+            int num = input % 10;
+            if (arrayNum[num] != 0 || num == 0) {
+                return false;
+            }
+            arrayNum[num] = 1;
+            input = input / 10;
+        }
+        return true;
+    }
+
     // 사용자가 범위 내의 수를 입력했는지 확인
     private static String inputUser() {
         String input = Console.readLine();
         try {
             int inputNum = Integer.parseInt(input);
-
             if (123 > inputNum || inputNum > 987) {
-                System.out.println("ERROR");
+                printError();
+                return "ERROR";
+            }
+            if (!checkInvalidate(inputNum)) {
+                printError();
                 return "ERROR";
             }
         } catch (NumberFormatException e) {
-            System.out.println("ERROR");
+            printError();
             return "ERROR";
         }
 
         return input;
     }
+
+    // 사용자 입력이 잘 못 됐을 경우 출력하는 함수
+    private static void printError() {
+        System.out.println("ERROR");
+    }
+
 }
