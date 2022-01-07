fe06289650c8c7f6799617316956af3db49a8deb
1101717 <a1101717@1101717M01.local>
2022-01-07 15:01:51 +0900

    feat: add game start title & user's input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import nextstep.utils.Randoms;
+import nextstep.utils.Console;
 
 public class Application {
 
@@ -11,8 +12,17 @@ public class Application {
         boolean isRunned = true;
 
         while (isRunned) {
+            System.out.println("⚾⚾⚾⚾⚾⚾");
+            System.out.println("\uD83D\uDC9B\uD83D\uDC9B\uD83D\uDC9B\uD83D\uDC9B\uD83D\uDC9B\uD83D\uDC9B");
+            System.out.println("⚾⚾⚾⚾⚾⚾");
+            System.out.println();
+
             // 1 이상 9 이하의 임의의 수 생성
             int computerNumber = Randoms.pickNumberInRange(1,9);
+
+            // 사용자 수 입력
+            System.out.printf("숫자를 입력해 주세요⚾️ : ");
+            String userInput = Console.readLine();
 
             // 게임 시
         }
