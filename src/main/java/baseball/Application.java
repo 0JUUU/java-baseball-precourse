2b8d9c729d788aea54d38ebd1686fe2b1f7f90a3
1101717 <a1101717@1101717M01.local>
2022-01-07 16:02:50 +0900

    feat: add game finish

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,9 +9,9 @@ public class Application {
         // TODO 숫자 야구 게임 구현
 
         // 게임 상태 관리 변수
-        boolean isRunned = true;
+        boolean isClosed = false;
 
-        while (isRunned) {
+        while (!isClosed) {
             // 게임 타이틀
             System.out.println("⚾⚾⚾⚾⚾⚾");
             System.out.println(
@@ -21,30 +21,61 @@ public class Application {
 
             // 1 이상 9 이하의 임의의 수 생성
             int computerNumber = Randoms.pickNumberInRange(123, 987);
-            while (!checkInvalidate(computerNumber)) {
+            while (!checkInvalidate(computerNumber, 123, 987)) {
                 computerNumber = Randoms.pickNumberInRange(123, 987);
             }
 
-            // 사용자 수 입력
-            String userInput = "ERROR";
+            boolean isCorrect = false;
+            while (!isCorrect) {
+                // 사용자 수 입력
+                String userInput = "ERROR";
 
-            while (userInput.equals("ERROR")) {
-                System.out.printf("숫자를 입력해 주세요⚾️ : ");
-                userInput = inputUser();
+                while (userInput.equals("ERROR")) {
+                    System.out.printf("숫자를 입력해 주세요⚾️ : ");
+                    userInput = inputUser();
+                }
+                int input = Integer.parseInt(userInput);
+                System.out.println(computerNumber + " / " + input);
+
+                // 비교
+                isCorrect = checkNumber(computerNumber, input);
             }
-            int input = Integer.parseInt(userInput);
-            System.out.println(computerNumber + " / " + input);
-            // 비교
-            // String info = checkNumber(computerNumber, userInput);
 
-            // 게임 시
+
+            // 게임 진행 여부 물어보기
+            int gameNumber = askGameRestart();
+            if (gameNumber == 2) {
+                isClosed = true;
+            }
         }
-
+        System.out.println("게임 끝");
     }
 
-    // 123 이상 987 이하 & 서로 다른 세 수인지 & 0 포함 여부 확인
-    private static boolean checkInvalidate(int input) {
-        if (123 > input || input > 987) {
+    private static int askGameRestart() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        while (true) {
+            try {
+                int gameInput = Integer.parseInt(Console.readLine());
+                if(checkInvalidate(gameInput, 1, 2))
+                    return gameInput;
+                printError();
+            } catch (NumberFormatException e) {
+                printError();
+            }
+        }
+    }
+
+    // 숫자 야구 진행 (스트라이크, 볼, 낫씽 여부 판단)
+    private static boolean checkNumber(int computerNumber, int input) {
+        if (computerNumber == input) return true;
+        else return false;
+    }
+
+    // 시작 범위 이상 끝 범위 이하 & 서로 다른 세 수인지 & 0 포함 여부 확인
+    private static boolean checkInvalidate(int input, int start, int end) {
+        if (start > input || input > end) {
             return false;
         }
         int[] arrayNum = new int[10];
@@ -64,11 +95,7 @@ public class Application {
         String input = Console.readLine();
         try {
             int inputNum = Integer.parseInt(input);
-            if (123 > inputNum || inputNum > 987) {
-                printError();
-                return "ERROR";
-            }
-            if (!checkInvalidate(inputNum)) {
+            if (!checkInvalidate(inputNum, 123, 987)) {
                 printError();
                 return "ERROR";
             }
@@ -80,7 +107,7 @@ public class Application {
         return input;
     }
 
-    // 사용자 입력이 잘 못 됐을 경우 출력하는 함수
+    // 사용자 입력이 잘못 됐을 경우 출력하는 함수
     private static void printError() {
         System.out.println("ERROR");
     }
